8307ab1c52350d2a9d73dd69beb83d32391160ba
Bob Rubbens <bobrubbens@gmail.com>
2022-01-24 17:35:02 +0100

    Different approach

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,21 +1,25 @@
 const core = require("@actions/core");
 const { Octokit } = require("@octokit/action");
 
-const firstGreeting = core.getInput("first-greeting");
-const secondGreeting = core.getInput("second-greeting");
-const thirdGreeting = core.getInput("third-greeting");
+const main = async () => {
+	const firstGreeting = core.getInput("first-greeting");
+	const secondGreeting = core.getInput("second-greeting");
+	const thirdGreeting = core.getInput("third-greeting");
 
-console.log(`Hello ${firstGreeting}`);
-console.log(`Hello ${secondGreeting}`);
-if (thirdGreeting) {
-    console.log(`Hello ${thirdGreeting}`);
-}
+	console.log(`Hello ${firstGreeting}`);
+	console.log(`Hello ${secondGreeting}`);
+	if (thirdGreeting) {
+		console.log(`Hello ${thirdGreeting}`);
+	}
 
-const octokit = new Octokit();
+	const octokit = new Octokit();
 
-const pulls = await octokit.rest.pulls.list({
-	owner: "utwente_fmt",
-	repo: "vercors",
-});
+	const pulls = await octokit.rest.pulls.list({
+		owner: "utwente_fmt",
+		repo: "vercors",
+	});
 
-console.log(pulls);
+	console.log(pulls);
+};
+
+main();
