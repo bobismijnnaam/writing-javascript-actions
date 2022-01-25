8e7da02a07eaa343027c7aad607a43d9de450585
Bob Rubbens <bobrubbens@gmail.com>
2022-01-25 17:11:08 +0100

    Work out design

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,14 +1,30 @@
 const core = require("@actions/core");
 const github = require('@actions/github');
 
-// Workflow:
-// Get current commit
-// Find PR that has as base branch the current branch
-// If mergable, exit
-// If not, continue
-// If unknown, wait 10 seconds, and try again every second
-//
-// When multiple commits are pushed, only the latest gets executed in an action. Additionally, it is assumed in this action we are not dealing with technically annoying behaviour, i.e., pushing single branches rapidly. Therefore it is safe to assume that we can wait 10 sec to check if the current branch is mergable. In the worst case, 10 seconds is wasted waiting
+/*
+Workflow
+currentCommit, currentBranch, currentEvent
+
+if currentEvent != push:
+	This workflow is allowed to run, because it is not triggered by a push event, but a currentEvent
+
+prs = getPrs()
+prs = [pr for pr in prs if branch(pr) == currentBranch]
+
+while, at least once, there are PRs with mergable not yet set, or 10s have passed:
+	if exists pr, head(currentBranch) == currentCommit && !mergable(pr)
+		This workflow must run, as there is at least one case where the pull_request event does not run because the commit is not mergable
+
+	if all pr, branch(pr) == currentBranch ==>
+			head(currentBranch) == currentCommit && mergable(pr)
+		This workflow can be safely canceled
+	
+	sleep(1s)
+
+At this point, there are commits past currentCommit, so we cannot rely on the mergable flag.
+Therefore, when in doubt, we let it continue
+
+*/
 
 const main = async () => {
 	const myToken = core.getInput('GITHUB_TOKEN');
