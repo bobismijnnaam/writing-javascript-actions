baf05c263d6fffc161a9c9135a93e7edc6c3e3fc
Bob Rubbens <bobrubbens@gmail.com>
2022-01-27 15:24:14 +0100

    Add some debug statements

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -36,12 +36,14 @@ const secondsToNanos = x => x * 1000000000n; // Returns bigint because of "n" at
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 async function getHeadOf(octokit, owner, repo, branch) {
-	const { object: { sha } } = await octokit.rest.git.getRef({
+	const obj = await octokit.rest.git.getRef({
+	// const { object: { sha } } = await octokit.rest.git.getRef({
 		owner,
 		repo,
 		ref: "heads/" + branch,
 	});
-	return sha;
+	console.log(obj);
+	return obj["object"]["sha"];
 }
 
 const main = async () => {
